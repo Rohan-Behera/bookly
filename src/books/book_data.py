7a6180988525d9852c201acd014c18e83ae89b46
Rohan Kumar Behera <rohan.behera@cozentus.com>
2026-05-28 15:41:52 +0530

    Added new module books

---
+++ src/books/book_data.py
@@ -0,0 +1,47 @@
+books = [
+            {
+                "id": 1,
+                "title": "The Great Gatsby",
+                "author": "F. Scott Fitzgerald",
+                "publisher": "Scribner",
+                "published_date": "1925-04-10",
+                "page_count": 180,
+                "language": "English"
+            },
+            {
+                "id": 2,
+                "title": "To Kill a Mockingbird",
+                "author": "Harper Lee",
+                "publisher": "J. B. Lippincott & Co.",
+                "published_date": "1960-07-11",
+                "page_count": 281,
+                "language": "English"
+            },
+            {
+                "id": 3,
+                "title": "1984",
+                "author": "George Orwell",
+                "publisher": "Secker & Warburg",
+                "published_date": "1949-06-08",
+                "page_count": 328,
+                "language": "English"
+            },
+            {
+                "id": 4,
+                "title": "The Alchemist",
+                "author": "Paulo Coelho",
+                "publisher": "HarperCollins",
+                "published_date": "1988-01-01",
+                "page_count": 208,
+                "language": "Portuguese"
+            },
+            {
+                "id": 5,
+                "title": "The Pragmatic Programmer",
+                "author": "Andrew Hunt",
+                "publisher": "Addison-Wesley",
+                "published_date": "1999-10-20",
+                "page_count": 352,
+                "language": "English"
+            }
+        ]
