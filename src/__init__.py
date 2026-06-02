997d84e2cb3889f947d34a9235e4a65930f2ef09
Rohan Kumar Behera <rohan.behera@cozentus.com>
2026-06-02 16:57:28 +0530

    Middleware

--- src/__init__.py
+++ src/__init__.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, status
+from fastapi import FastAPI
 from contextlib import asynccontextmanager
 from src.db.main import init_db
 
@@ -9,22 +9,8 @@ from src.reviews.routes import review_router
 from src.tags.routers import tags_router
 
 #Errors
-from .errors import (
-    BooklyException,
-    AccessTokenRequired,
-    AccountNotVerified,
-    BookNotFound,
-    InsufficientPermission,
-    InvalidCredentials,
-    InvalidToken,
-    RefreshTokenRequired,
-    RevokedToken,
-    TagAlreadyExists,
-    TagNotFound,
-    UserAlreadyExists,
-    UserNotFound,
-    create_exception_handler
-)
+from .errors import register_all_errors
+from.middleware import register_middleware
 
 #life span event
 @asynccontextmanager
@@ -43,138 +29,10 @@ app = FastAPI(
 )
 
 # Registering exception handlers
-app.add_exception_handler(
-    UserAlreadyExists,
-    create_exception_handler(
-        status_code=status.HTTP_403_FORBIDDEN,
-        message={
-            "message": "User with email already exits",
-            "error_code": "User_exists"
-        }
-    )
-)
+register_all_errors(app)
 
-app.add_exception_handler(
-    InvalidToken,
-    create_exception_handler(
-        status_code=status.HTTP_401_UNAUTHORIZED,
-        message={
-            "message": "Invalid token provided",
-            "error_code": "Invalid_token"
-        }
-    )
-)
-
-app.add_exception_handler(
-    RevokedToken,
-    create_exception_handler(
-        status_code=status.HTTP_401_UNAUTHORIZED,
-        message={
-            "message": "Token has been revoked",
-            "error_code": "Revoked_token"
-        }
-    )
-)
-
-app.add_exception_handler(
-    AccessTokenRequired,
-    create_exception_handler(
-        status_code=status.HTTP_401_UNAUTHORIZED,
-        message={
-            "message": "Access token required",
-            "error_code": "Access_token_required"
-        }
-    )
-)
-
-app.add_exception_handler(
-    RefreshTokenRequired,
-    create_exception_handler(
-        status_code=status.HTTP_401_UNAUTHORIZED,
-        message={
-            "message": "Refresh token required",
-            "error_code": "Refresh_token_required"
-        }
-    )
-)
-
-app.add_exception_handler(
-    InvalidCredentials,
-    create_exception_handler(
-        status_code=status.HTTP_403_FORBIDDEN,
-        message={
-            "message": "Invalid email or password",
-            "error_code": "Invalid_credentials"
-        }
-    )
-)
-
-app.add_exception_handler(
-    InsufficientPermission,
-    create_exception_handler(
-        status_code=status.HTTP_403_FORBIDDEN,
-        message={
-            "message": "Insufficient permissions",
-            "error_code": "Insufficient_permission"
-        }
-    )
-)
-
-app.add_exception_handler(
-    BookNotFound,
-    create_exception_handler(
-        status_code=status.HTTP_404_NOT_FOUND,
-        message={
-            "message": "Book not found",
-            "error_code": "Book_not_found"
-        }
-    )
-)
-
-app.add_exception_handler(
-    TagNotFound,
-    create_exception_handler(
-        status_code=status.HTTP_404_NOT_FOUND,
-        message={
-            "message": "Tag not found",
-            "error_code": "Tag_not_found"
-        }
-    )
-)
-
-app.add_exception_handler(
-    TagAlreadyExists,
-    create_exception_handler(
-        status_code=status.HTTP_403_FORBIDDEN,
-        message={
-            "message": "Tag already exists",
-            "error_code": "Tag_exists"
-        }
-    )
-)
-
-app.add_exception_handler(
-    UserNotFound,
-    create_exception_handler(
-        status_code=status.HTTP_404_NOT_FOUND,
-        message={
-            "message": "User not found",
-            "error_code": "User_not_found"
-        }
-    )
-)
-
-app.add_exception_handler(
-    AccountNotVerified,
-    create_exception_handler(
-        status_code=status.HTTP_403_FORBIDDEN,
-        message={
-            "message": "Account not verified",
-            "error_code": "Account_not_verified"
-        }
-    )
-)
-
+# middleware
+register_middleware(app)
 
 app.include_router(book_router, prefix=f"/api/{version}/books", tags=['books'])
 app.include_router(auth_router, prefix=f'/api/{version}/auth', tags=['auth'])
---
+++ src/middleware.py
@@ -0,0 +1,57 @@
+from fastapi import FastAPI, status
+from fastapi.requests import Request
+from fastapi.responses import JSONResponse
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.middleware.trustedhost import TrustedHostMiddleware
+import time
+import logging
+
+logger = logging.getLogger('uvicorn.access')
+logger.disabled = True
+
+
+# In middleware we cannot raise exceptions we can only return response.
+# we can modify the response such that we can show exception errors using JSONResponse
+
+
+def register_middleware(app: FastAPI):
+
+    @app.middleware('http')
+    async def custom_logging(request: Request, call_next):
+        start_time = time.time()
+        response = await call_next(request)
+        processing_time = time.time() - start_time
+        message = f"{request.method} - {request.url.path} - {response.status_code} completes after{processing_time}s"
+        print(message)
+        return response
+    
+    app.add_middleware(CORSMiddleware,
+        allow_origins = ["*"], 
+        allow_methods = ["*"], 
+        allow_headers = ["*"],
+        allow_credentials = True,
+    )
+
+    app.add_middleware(TrustedHostMiddleware,
+                       allowed_hosts=[""]
+                       )
+    
+
+    """
+    Below is just an example for the authorization middleware it is similar to that of dependecy injection in FASTAPI.
+    Always remember If you only need logic for some routes (like specific admin panels), use FastAPI Dependencies. 
+    Middleware is "heavy" because it runs on every single call to your server.
+    """
+    # @app.middleware('http')
+    # async def authorization(request: Request, call_next):
+    #     if not "Authorization" in request.headers:
+    #         return JSONResponse(
+    #             content={
+    #                 "message" : "not authenticated",
+    #                 "resolution" : "please provide the right credentials to proceed"
+    #             },
+    #             status_code=status.HTTP_401_UNAUTHORIZED
+    #         )
+        
+    #     response = await call_next(request)
+    #     return response
